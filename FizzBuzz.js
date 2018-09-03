161b52c0670e8015897adb89fe4dffa12ac74fb7
Jeff Innes <jeffdango@gmail.com>
2018-09-03 12:28:42 -0700

    Cleaned out the testing bits

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,46 +1,18 @@
-const fs = require('fs');
-
-let answerKey = fs.readFileSync('FizzBuzz_answer_key.txt').toString().split(',');
-let answer = [];
-
-
 for (let i = 1; i < 101; i++) {
     let str = '';
 
     if ( i % 3 === 0 ) {
-
         str = str + 'Fizz';
-
     }
 
     if ( i % 5 === 0) {
-
         str = str + 'Buzz';
-
     }
 
     if ( str === '' ) {
-
         str = i.toString();
-
     }
 
-    answer.push(str);
+    console.log('str');
     
 }
-
-
-
-// Really hacky way to check my answer. #notproud
-
-if ( answer.length === answerKey.length ) {
-
-    console.log('answer  -  answerKey');
-
-    for (let j = 0; j < answer.length; j++) {
-        
-            console.log(answer[j] + '  -  ' + answerKey[j] + '\n');
-        
-    }
-
-} 
